5d5e6dad2e368ab7fd651ef0ffdf2b41860186a7
pcbimon <patipat.che@mahidol.edu>
2024-06-17 10:59:06 +0700

    refract: remove unuse code

--- ckanext/oauth2/controller.py
+++ ckanext/oauth2/controller.py
@@ -60,9 +60,6 @@ class OAuth2Controller(base.BaseController):
         try:
             token = self.oauth2helper.get_token()
             user_name = self.oauth2helper.identify(token)
-            log.debug(f'User {user_name} identified')
-            # user = toolkit.get_action('user_show')(data_dict={'id': user_name})
-            # log.debug(f'User {user_name} found in CKAN')
             self.oauth2helper.remember(user_name)
             self.oauth2helper.update_token(user_name, token)
             self.oauth2helper.redirect_from_callback()
