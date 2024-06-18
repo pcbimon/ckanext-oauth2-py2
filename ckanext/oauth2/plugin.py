6e9c215dfb9d8bbf727104a28b1510258f456cec
pcbimon <patipat.che@mahidol.edu>
2024-06-18 10:27:08 +0700

    feat: disable user update

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -173,7 +173,7 @@ class OAuth2Plugin(plugins.SingletonPlugin):
         # we need to prevent some actions being authorized.
         return {
             'user_create': user_create,
-            'user_update': user_update,
+            # 'user_update': user_update,
             'user_reset': user_reset,
             'request_reset': request_reset
         }
