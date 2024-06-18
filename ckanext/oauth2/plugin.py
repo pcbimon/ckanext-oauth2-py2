32c61d580a3020ed83e114c94066943cc3d01048
pcbimon <patipat.che@mahidol.edu>
2024-06-18 10:58:56 +0700

    feat: update user edit view

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -126,7 +126,8 @@ class OAuth2Plugin(plugins.SingletonPlugin):
         # Redirect the user to the OAuth service reset page
         if self.edit_url:
             m.redirect('/user/edit/{user}', self.edit_url)
-
+        # overide edit view user
+        map.connect('user_edit', '/user/edit', controller='user', action='edit', template='/user/edit_user_form.html')
         return m
     def identify(self):
         log.debug('identify')
