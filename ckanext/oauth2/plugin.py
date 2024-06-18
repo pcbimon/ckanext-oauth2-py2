519b86f2acfd82768df1beff789ac63e821bb64c
pcbimon <patipat.che@mahidol.edu>
2024-06-18 14:36:24 +0700

    feat: enable user create function

--- ckanext/oauth2/plugin.py
+++ ckanext/oauth2/plugin.py
@@ -200,7 +200,7 @@ class OAuth2Plugin(plugins.SingletonPlugin):
     def get_auth_functions(self):
         # we need to prevent some actions being authorized.
         return {
-            'user_create': user_create,
+            # 'user_create': user_create,
             # 'user_update': user_update,
             'user_reset': user_reset,
             'request_reset': request_reset
